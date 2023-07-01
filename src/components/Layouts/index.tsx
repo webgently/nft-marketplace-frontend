cfc4e6210acdff6322200e4d03bddc18c975f5fb
webgently <oleksandrshkabura811@gmail.com>
2023-07-01 19:20:15 +0300

    feat: layouts component

---
+++ src/components/Layouts/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layouts = ({ children }: ComPropsObject) => {
+  return (
+    <div className="min-h-100vh flex flex-col bg-primary text-white">
+      <Header />
+      <div className="flex-1 flex flex-col">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+export default Layouts;
