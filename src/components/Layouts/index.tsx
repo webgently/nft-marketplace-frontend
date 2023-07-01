98aa266a1f9bdb4755c78a3c6ce20929654e8885
webgently <oleksandrshkabura811@gmail.com>
2023-07-01 20:02:05 +0300

    update: layouts

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -6,7 +6,7 @@ const Layouts = ({ children }: ComPropsObject) => {
   return (
     <div className="min-h-100vh flex flex-col bg-primary text-white">
       <Header />
-      <div className="flex-1 flex flex-col">{children}</div>
+      <div className="flex-1 flex flex-col pt-[100px]">{children}</div>
       <Footer />
     </div>
   );
