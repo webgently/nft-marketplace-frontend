05db4e5b9779c10bde594febad88dcfdb1436d70
webgently <oleksandrshkabura811@gmail.com>
2023-07-01 19:11:49 +0300

    feat: add menu or socials list and rename component name

---
+++ src/components/Layouts/Header/list.config.tsx
@@ -0,0 +1,35 @@
+const menuLists = [
+  {
+    label: 'Marketplace',
+    url: '/marketplace'
+  },
+  {
+    label: 'Rankings',
+    url: '/rankings'
+  },
+  {
+    label: 'Connect a wallet',
+    url: '/connect-wallet'
+  }
+];
+
+const socialLists = [
+  {
+    icon: 'Discord',
+    link: '/'
+  },
+  {
+    icon: 'Youtube',
+    link: '/'
+  },
+  {
+    icon: 'Twitter',
+    link: '/'
+  },
+  {
+    icon: 'Instagram',
+    link: '/'
+  }
+];
+
+export { menuLists, socialLists };
