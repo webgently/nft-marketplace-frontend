e26035b2a1cc9d5449b11ffe9dabc02a2a82cb70
webgently <oleksandrshkabura811@gmail.com>
2023-07-03 08:51:41 +0300

    feat: add nft card component

---
+++ src/components/NftCard/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './nftcard.scss';
+
+const NftCard = ({ ind, art, name, avatar, author, price, currency, highestBid, bidCurrency }: any) => {
+  return (
+    <div className="nft-card">
+      <img src={art} alt={'nft' + ind} />
+      <div className="nft-detail">
+        <div className="creator-detail">
+          <p>{name}</p>
+          <div className="flex items-center gap-[12px]">
+            <img src={avatar} width="24px" alt={'creator' + ind} />
+            <span>{author}</span>
+          </div>
+        </div>
+        <div className="price-detail">
+          <div className="flex flex-col items-start gap-[8px]">
+            <span className="text-third !text-[14px]">Price:</span>
+            <span>
+              {price.toFixed(2)} {currency}
+            </span>
+          </div>
+          <div className="flex flex-col items-end gap-[8px]">
+            <span className="text-third !text-[12px]">Highest Bid:</span>
+            <span>
+              {highestBid.toFixed(2)} {bidCurrency}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NftCard;
