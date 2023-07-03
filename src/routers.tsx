d4fb3c6e6fc04b647d5be4b1e7443e263bf56f4f
webgently <oleksandrshkabura811@gmail.com>
2023-07-03 20:05:43 +0300

    feat: add marketplace router

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NotificationContainer } from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
 import Home from './pages/Home';
+import Marketplace from './pages/Marketplace';
+import 'react-notifications/lib/notifications.css';
 
 export const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/marketplace" element={<Marketplace />} />
       </Routes>
-
       <NotificationContainer />
     </BrowserRouter>
   );
