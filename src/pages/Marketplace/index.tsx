8d87339c166962a3186845d743f8d18cdbaa48d6
webgently <oleksandrshkabura811@gmail.com>
2023-07-03 20:05:25 +0300

    feat: init marketplace page

---
+++ src/pages/Marketplace/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './marketplace.scss';
+import Layouts from '../../components/Layouts';
+
+const Marketplace = () => {
+  return <Layouts></Layouts>;
+};
+
+export default Marketplace;
