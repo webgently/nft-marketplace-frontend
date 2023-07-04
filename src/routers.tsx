7f8d66028172752590847b0f602a4f4515f8fb7d
webgently <oleksandrshkabura811@gmail.com>
2023-07-04 10:00:15 +0300

    feat: add any routers

--- src/routers.tsx
+++ src/routers.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NotificationContainer } from 'react-notifications';
 import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
+import Collection from './pages/Collection';
+import Profile from './pages/Profile';
 import 'react-notifications/lib/notifications.css';
 
 export const Routers = () => {
@@ -11,6 +13,8 @@ export const Routers = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/rankings" element={<Collection />} />
+        <Route path="/connect-wallet" element={<Profile />} />
       </Routes>
       <NotificationContainer />
     </BrowserRouter>
